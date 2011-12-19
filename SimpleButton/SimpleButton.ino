b8ff6e721aca66c551a8d74f47cabc9ec3966919
Daniel Gomes <daniel.gomes@reset.pt>
2011-12-19 23:09:16 +0000

    Added Simple Button

---
+++ SimpleButton/SimpleButton.ino
@@ -0,0 +1,21 @@
+/*
+ * A simple button to switch the led on/off
+ *
+ * 2011 © Daniel Gomes <me@danielcsgomes.com>
+ */
+
+int pinRead = 4;
+int pinOut = 4;
+
+void setup()
+{
+  // initialize the digitals pins one as an output and other as input
+  pinMode(pinRead, INPUT);
+  pinMode(pinOut, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(pinOut, HIGH); // set the led on
+  Serial.println(digitalRead(pinRead)); // print the current state of the button 1 - on , 0 - off
+}
